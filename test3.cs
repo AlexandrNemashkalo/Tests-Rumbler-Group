1ccb6d5dd8fb3e9eaafe20428f49935ee0a84ac1
Alexandr Nemashkalo <60902736+AlexandrNemashkalo@users.noreply.github.com>
2020-10-08 19:27:43 +0300

    Update test3.cs

--- test3.cs
+++ test3.cs
@@ -4,49 +4,52 @@
 //Каждый раз переписывать метод Process, расширять swich, добавлять приватные методы ProcessJson, ProcessXml  и т.п.? 
 public class FileService
 {
-	enum TextType
-	{
-    		HtmlCode,
-    		RawText
-	}
-	public void Process(string filePath)
-	{
+    enum TextType
+    {
+    	HtmlCode,
+    	RawText
+    }
+	
+    public void Process(string filePath)
+    {
     	var streamReader = new StreamReader(File.OpenRead(filePath));
     	var text = streamReader.ReadToEnd();
    	 
     	if (text.IndexOf("<html") != -1)
     	{
-        	ProcessHtmlCode(text);
+            ProcessHtmlCode(text);
     	}
     	else
     	{
-        	ProcessRawText(text);
+            ProcessRawText(text);
     	}
-
     	ProcessText(
-        	text,
-        	text.IndexOf("<html") != -1
-            	? TextType.HtmlCode
-            	: TextType.RawText);
-   	 
+            text,
+            text.IndexOf("<html") != -1
+            ? TextType.HtmlCode
+            : TextType.RawText);
+   
     	streamReader.Close();
-	}
-	private void ProcessText(string text, TextType textType)
-	{
+    }
+	
+    private void ProcessText(string text, TextType textType)
+    {
     	switch (textType)
     	{
-        	case TextType.HtmlCode:
-            		ProcessHtmlCode(text);
-            		break;
-        	case TextType.RawText:
-            		ProcessRawText(text);
-            		break;
-        	default:
-            		throw new Exception("Unknown file format");
+            case TextType.HtmlCode:
+                ProcessHtmlCode(text);
+                break;
+            case TextType.RawText:
+                ProcessRawText(text);
+                break;
+            default:
+                throw new Exception("Unknown file format");
     	}
-	}
-	private void ProcessHtmlCode(string content){/*реализация метода*/}
-	private void ProcessRawText(string content){/*реализация метода*/}
+    }
+	
+    private void ProcessHtmlCode(string content){/*реализация метода*/}
+	
+    private void ProcessRawText(string content){/*реализация метода*/}
 }
 
 /*Ответ
@@ -59,39 +62,38 @@ public class FileService
 6.Здесь можно обойтись и без enum.
 */
 //Решение:
- public static class FileService
-    {     
-        public  static void Process(string filePath)
-        {
-            FileInfo fileInf = new FileInfo(filePath);
-            var streamReader = new StreamReader(File.OpenRead(filePath));
-            var text = streamReader.ReadToEnd();
+public static class FileService
+{     
+    public  static void Process(string filePath)
+    {
+         FileInfo fileInf = new FileInfo(filePath);
+         var streamReader = new StreamReader(File.OpenRead(filePath));
+         var text = streamReader.ReadToEnd();
 
-            switch (fileInf.Extension)
-            {
-                case ".htm":
-                    ProcessHtmlCode(text);
-                    break;
-                case ".txt":
-                    ProcessRawText(text);
-                    break;
-                default:
-                    throw new Exception("Unknown file format");
-            }
-            streamReader.Close();
-        }
-
-        private static void ProcessHtmlCode(string content)
-        {
-            Console.WriteLine("Обработан Html файл");
-        }
-
-        private static void ProcessRawText(string content)
-        {
-            Console.WriteLine("Обработан Txt файл");
-        }
-    
+         switch (fileInf.Extension)
+         {
+             case ".htm":
+                 ProcessHtmlCode(text);
+                 break;
+             сase ".txt":
+                 ProcessRawText(text);
+                 break;
+             default:
+                 throw new Exception("Unknown file format");
+	 }
+         streamReader.Close();
     }
+
+    private static void ProcessHtmlCode(string content)
+    {
+        Console.WriteLine("Обработан Html файл");
+    }
+
+    private static void ProcessRawText(string content)
+    {
+        Console.WriteLine("Обработан Txt файл");
+    }
+}
 
 /*возможное Решение:
 
